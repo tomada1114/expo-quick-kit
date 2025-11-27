516b727e492faf2619188e78d1fff37d3a60c08a
tomada <tmasuyama1114@gmail.com>
2025-11-27 15:52:59 -0600

    refactor: clean up migration and journal files by adding missing newlines and formatting
    - Added missing newlines at the end of migration and journal JSON files for consistency.
    - Reformatted the export structure in migrations.js for improved readability.

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,10 +3,9 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_wooden_quicksilver.sql';
 
-  export default {
-    journal,
-    migrations: {
-      m0000
-    }
-  }
-  
+export default {
+  journal,
+  migrations: {
+    m0000,
+  },
+};
