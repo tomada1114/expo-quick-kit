4076827159479556c1741dc5c7da0955b6ad7654
tomada <tmasuyama1114@gmail.com>
2025-11-27 14:52:01 -0600

    chore: ESLintとPrettierの設定を構築し、依存関係を更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+  prettierConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'build/*', 'node_modules/*', '.expo/*'],
   },
 ]);
